6bfc4290715514bd4a1c00312cf68cac9922686e
Barkin Koroglu <barkin_45_99@hotmail.com>
2023-06-28 16:38:37 +0300

    Register Modal

---
+++ airbnb/app/components/modals/RegisterModal.tsx
@@ -0,0 +1,59 @@
+'use client';
+import axios from 'axios';
+import { AiFillGithub } from 'react-icons/ai';
+// import { signIn } from 'next-auth/react';
+import { FcGoogle } from 'react-icons/fc';
+import { useCallback, useState } from 'react';
+import { toast } from 'react-hot-toast';
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+import useRegisterModal from '@/app/hooks/useRegisterModal';
+import Modal from './Modal';
+function RegisterModal() {
+  const registerModal = useRegisterModal();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: '',
+      email: '',
+      password: '',
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+
+    axios
+      .post('/api/register', data)
+      .then(() => {
+        toast.success('Registered!');
+        registerModal.onClose();
+      })
+      .catch((error) => {
+        toast.error(error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+
+  const onToggle = useCallback(() => {
+    registerModal.onClose();
+  }, [registerModal]);
+  return (
+    <Modal
+      disabled={isLoading}
+      isOpen={registerModal.isOpen}
+      title="Register"
+      actionLabel="Continue"
+      onClose={registerModal.onClose}
+      onSubmit={handleSubmit(onSubmit)}
+    />
+  );
+}
+
+export default RegisterModal;
