8aa5ac0718ba7ddb47796b6abfcdd79157a99a86
Barkin Koroglu <barkin_45_99@hotmail.com>
2023-06-30 15:09:16 +0300

    Toaster Provider

--- airbnb/app/components/modals/RegisterModal.tsx
+++ airbnb/app/components/modals/RegisterModal.tsx
@@ -70,6 +70,23 @@ function RegisterModal() {
         errors={errors}
         required
       />
+      <Input
+        id="name"
+        label="Name"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="password"
+        type="password"
+        label="Password"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
     </div>
   );
   return (
